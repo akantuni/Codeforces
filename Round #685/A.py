0fbda4c3e62c2a7f2494285aab3de8b67978c93a
Alex Kantuni <akantuni@gmail.com>
2020-11-23 21:35:45 -0500

    Add more solutions

--- Round #685/A.py
+++ Round #685/A.py
@@ -3,15 +3,10 @@ t = int(input())
 
 for i in range(t):
     n = int(input())
-    if n == 1:
-        print(0)
-    elif n == 2:
-        print(1)
-    elif n == 3:
-        print(2)
-    elif n % 2 == 1:
-        print(3)
+    if n <= 3:
+        print(n - 1)
     else:
-        print(2)
+        print(2 + (n & 1))
+
 
 
